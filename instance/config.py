7340afb481e5150b57fd1e0287d848cd17c0175c
shashidhar <sshashidhar2609@gmail.com>
2019-05-20 14:28:07 +0530

    first commit

---
+++ instance/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Sowmya@18@localhost/DB_NAME'
